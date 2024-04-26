7cdbf78aa819c0efcdf520435baed472b4be5718
vitoriMartins <110932209+vitoriMartins@users.noreply.github.com>
2024-04-26 07:42:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
+for(let i=0; i<botoes.length; i++){
+    botoes[i].onclick=function(){
+        botoes[i].classList.add("ativo");
+    }
+}
